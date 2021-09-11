82017dd8e585646de122c4b2be792733cf6ae8d9
Simon Reinhardt <simon.reinhardt@physik.uni-r.de>
2021-09-11 13:13:14 +0200

    inductor

--- parallel_wires/parallel_wires.m
+++ parallel_wires/parallel_wires.m
@@ -1,15 +1,4 @@
 #!/usr/bin/env -S octave --persist
-                %
-% Tutorials / MSL_NotchFilter
-%
-% Describtion at:
-% http://openems.de/index.php/Tutorial:_Microstrip_Notch_Filter
-%
-% Tested with
-%  - Matlab 2011a / Octave 4.0
-%  - openEMS v0.0.33
-%
-% (C) 2011-2015 Thorsten Liebig <thorsten.liebig@gmx.de>
 
 pkg load openems;
 pkg load csxcad;
@@ -23,17 +12,12 @@ MSL_width = 600;
 substrate_thickness = 254;
 substrate_epr = 3.66;
 
-
 f_max = 7e9;
 
-
-%% coil properties
-separation = 10e3
+%% metal wires
+wire_separation = 1000
 wire_radius = 100
-num_loops = 10
-loop_radius = 3e3
-lead_height = 3 * loop_radius
-coil_res = 100
+wire_height = 10e3
 
 
 
@@ -46,13 +30,11 @@ FDTD = SetBoundaryCond( FDTD, BC );
 %% setup CSXCAD geometry & mesh %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 CSX = InitCSX();
 resolution = c0/(f_max*sqrt(substrate_epr))/unit /50; % resolution of lambda/50
-
-mesh.x = SmoothMeshLines([-1.5*separation, 1.5*separation], resolution/4);
-mesh.x = SmoothMeshLines( [-MSL_length mesh.x MSL_length], resolution);
-
-mesh.y = SmoothMeshLines( [-15*MSL_width 15*MSL_width], resolution/4, 1.3 ,0);
-mesh.z = SmoothMeshLines( [linspace(0,substrate_thickness,5), 2*lead_height], resolution/4 );
-CSX = DefineRectGrid( CSX, unit, mesh );
+mesh.x = SmoothMeshLines( [0, 1.5*wire_separation], resolution/4, 1.5 ,0 );
+mesh.x = SmoothMeshLines( [-MSL_length -mesh.x mesh.x MSL_length], resolution, 1.5 ,0 );
+mesh.y = SmoothMeshLines( [0, MSL_width/2+[-resolution/3 +resolution/3*2]/4], resolution/4 , 1.5 ,0);
+mesh.y = SmoothMeshLines( [-15*MSL_width, -mesh.y, mesh.y,  15*MSL_width], resolution/4);
+mesh.z = SmoothMeshLines([0, substrate_thickness, 1.5*wire_height], resolution/4);
 
 %% substrate
 CSX = AddMaterial( CSX, 'RO4350B' );
@@ -61,50 +43,48 @@ start = [mesh.x(1),   mesh.y(1),   0];
 stop  = [mesh.x(end), mesh.y(end), substrate_thickness];
 CSX = AddBox( CSX, 'RO4350B', 0, start, stop );
 
-%% MSL port
 CSX = AddMetal( CSX, 'PEC' );
-portstart = [ mesh.x(1), -MSL_width/2, substrate_thickness];
-portstop  = [ -separation/2+4*wire_radius,  MSL_width/2, 0];
-[CSX,port{1}] = AddMSLPort( CSX, 999, 1, 'PEC', portstart, portstop, 0, [0 0 -1], 'ExcitePort', true, 'FeedShift', 10*resolution, 'MeasPlaneShift',  MSL_length/3);
 
-portstart = [mesh.x(end), -MSL_width/2, substrate_thickness];
-portstop  = [separation/2-4*wire_radius         ,  MSL_width/2, 0];
-[CSX,port{2}] = AddMSLPort( CSX, 999, 2, 'PEC', portstart, portstop, 0, [0 0 -1], 'MeasPlaneShift',  MSL_length/3 );
-
-%% wire coil
-count = 0
-p(1,1) = -separation/2;
+%% wires
+p(1,1) = -wire_separation/2;
 p(2,1) = 0;
 p(3,1) = substrate_thickness;
 
-## p(1,2) = -separation/2;
-## p(2,2) = 0;
-## p(3,2) = substrate_thickness + lead_height;
+p(1,2) = -wire_separation/2;
+p(2,2) = 0;
+p(3,2) = substrate_thickness + wire_height;
 
-dt = 1.0 / coil_res;
-
-count = 1;
-for n=0:num_loops-1
-    for m=0:coil_res
-      count = count + 1;
-      p(1,count) = -separation/2 + n*(separation / num_loops) + m*(separation / num_loops / coil_res);
-      p(2,count) = loop_radius * cos(2*pi*dt*m);
-      p(3,count) = substrate_thickness + lead_height + loop_radius * sin(2*pi*dt*m);
-    end
-end
-## count = count+1
-## p(1,count) = separation/2;
-## p(2,count) = 0;
-## p(3,count) = substrate_thickness + lead_height;
-count = count+1
-p(1,count) = separation/2;
-p(2,count) = 0;
-p(3,count) = substrate_thickness;
 CSX = AddWire(CSX, 'PEC', 0, p, wire_radius );
 
+p(1,1) = wire_separation/2;
+p(2,1) = 0;
+p(3,1) = substrate_thickness;
 
+p(1,2) = wire_separation/2;
+p(2,2) = 0;
+p(3,2) = substrate_thickness + wire_height;
+
+CSX = AddWire(CSX, 'PEC', 0, p, wire_radius );
+
+CSX = DefineRectGrid( CSX, unit, mesh );
+
+
+%% MSL port
+
+portstart = [ mesh.x(1), -MSL_width/2, substrate_thickness];
+portstop  = [ -(wire_separation/2 - 2*wire_radius),  MSL_width/2, 0];
+[CSX,port{1}] = AddMSLPort( CSX, 999, 1, 'PEC', portstart, portstop, 0, [0 0 -1], 'ExcitePort', true, 'FeedShift', 10*resolution, 'MeasPlaneShift',  MSL_length/3);
+
+portstart = [mesh.x(end), -MSL_width/2, substrate_thickness];
+portstop  = [(wire_separation/2 - 2*wire_radius),  MSL_width/2, 0];
+[CSX,port{2}] = AddMSLPort( CSX, 999, 2, 'PEC', portstart, portstop, 0, [0 0 -1], 'MeasPlaneShift',  MSL_length/3 );
+
+
+
+
+ 
 %% write/show/run the openEMS compatible xml-file
-Sim_Path = 'tmp_notch_filter';
+Sim_Path = 'tmp';
 Sim_CSX = 'msl.xml';
 
 [status, message, messageid] = rmdir( Sim_Path, 's' ); % clear previous directory
@@ -112,7 +92,7 @@ Sim_CSX = 'msl.xml';
 
 WriteOpenEMS( [Sim_Path '/' Sim_CSX], FDTD, CSX );
 CSXGeomPlot( [Sim_Path '/' Sim_CSX] );
-RunOpenEMS( Sim_Path, Sim_CSX );
+RunOpenEMS( Sim_Path, Sim_CSX);
 
 %% post-processing
 close all
@@ -129,4 +109,4 @@ plot(f/1e9,20*log10(abs(s21)),'LineWidth',2);
 legend('S_{11}','S_{21}');
 ylabel('S-Parameter (dB)','FontSize',12);
 xlabel('frequency (GHz) \rightarrow','FontSize',12);
-ylim([-40 2]);
+## ylim([-40 2]);
